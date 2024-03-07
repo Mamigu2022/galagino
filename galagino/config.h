afb392263187d01441dac6fb9cc443a610f23614
Mamigu2022 <157837973+Mamigu2022@users.noreply.github.com>
2024-03-07 21:32:08 +0100

    Add files via upload

--- galagino/config.h
+++ galagino/config.h
@@ -29,54 +29,46 @@
 
 #include "dip_switches.h"
 
-// #define CHEAP_YELLOW_DISPLAY_CONF
-
-#ifndef CHEAP_YELLOW_DISPLAY_CONF // Config as it was before
-
-// video config
-#define TFT_SPICLK  40000000    // 40 Mhz. Some displays cope with 80 Mhz
-
-#define TFT_CS   5
-#define TFT_DC  32
-#define TFT_RST 27
-#define TFT_BL  13    // don't set if backlight is hard wired
-#define TFT_ILI9341   // define for ili9341, otherwise st7789
-#define TFT_VFLIP     // define for upside down
 
 // x and y offset of 224x288 pixels inside the 240x320 screen
 #define TFT_X_OFFSET  8
 #define TFT_Y_OFFSET 16
 
-#define LED_PIN        16   // pin used for optional WS2812 stripe
+//#define LED_PIN        16   // pin used for optional WS2812 stripe
 #define LED_BRIGHTNESS 50   // range 0..255
 
 // audio config
-// #define SND_DIFF   // set to output differential audio on GPIO25 _and_ inverted on GPIO26
-
+ //#define SND_DIFF   // set to output differential audio on GPIO25 _and_ inverted on GPIO26
+#define SND_LEFT_CHANNEL
 // Pins used for buttons
-#define BTN_START_PIN  22
-#define BTN_COIN_PIN   21   // if this is not defined, then start will act as coin & start
-#define BTN_LEFT_PIN   33
-#define BTN_RIGHT_PIN  14
-#define BTN_DOWN_PIN   15
-#define BTN_UP_PIN      4
-#define BTN_FIRE_PIN   12
-
-#else                       // Cheap Yellow Display Conf
+#define BTN_START_PIN  27
+#define BTN_COIN_PIN   32   // if this is not defined, then start will act as coin & start
+//#ifdef ODROID_GO
+//#define EJE_X          34
+//#define EJE_Y          35
+//#else
+#define BTN_LEFT_PIN   12
+#define BTN_RIGHT_PIN  0
+#define BTN_DOWN_PIN   4
+#define BTN_UP_PIN      2
+//#endif
+#define BTN_FIRE_PIN   13
+#define BUTTON_EXTRA    32
+//#define TFT_MAC  0x20  // some CYD need this to rotate properly and have correct colors
 
 // video config
 #define TFT_SPICLK 40000000 // 40 Mhz. Some displays cope with 80 Mhz
 
-#define TFT_MISO 12
-#define TFT_MOSI 13
-#define TFT_SCLK 14
+#define TFT_MISO -1
+#define TFT_MOSI 23
+#define TFT_SCLK 18
 
-#define TFT_CS 15
-#define TFT_DC 2
+#define TFT_CS 5
+#define TFT_DC 21
 #define TFT_RST -1
-#define TFT_BL 21   // don't set if backlight is hard wired
+#define TFT_BL 14   // don't set if backlight is hard wired
 #define TFT_ILI9341 // define for ili9341, otherwise st7789
-// #define TFT_VFLIP   // define for upside down
+ #define TFT_VFLIP   // define for upside down
 
 // #define TFT_MAC  0x20  // some CYD need this to rotate properly and have correct colors
 
@@ -84,24 +76,21 @@
 #define TFT_X_OFFSET 8
 #define TFT_Y_OFFSET 16
 
-#define LED_PIN 16        // pin used for optional WS2812 stripe
+//#define LED_PIN 16        // pin used for optional WS2812 stripe
 #define LED_BRIGHTNESS 50 // range 0..255
 
 // audio config (leave both commented out for GPIO 25 for Audio)
 // #define SND_DIFF   // set to output differential audio on GPIO25 _and_ inverted on GPIO26
 #define SND_LEFT_CHANNEL // Use GPIO 26 for audio
 
-#define NUNCHUCK_INPUT
+//#define NUNCHUCK_INPUT
 
-#define NUNCHUCK_SDA 22
-#define NUNCHUCK_SCL 27
+#define NUNCHUCK_SDA 16
+#define NUNCHUCK_SCL 17
 
 #define NUNCHUCK_MOVE_THRESHOLD 30 // This is the dead-zone for where minor movements on the stick will not be considered valid movements
 
 // Pins used for buttons
-#define BTN_START_PIN 0 //This is the "boot" button
-// #define BTN_COIN_PIN 21 // if this is not defined, then start will act as coin & start
 
-#endif // #ifndef CHEAP_YELLOW_DISPLAY_CONF
 
 #endif // _CONFIG_H_
--- galagino/emulation.h
+++ galagino/emulation.h
@@ -11,14 +11,13 @@ extern unsigned char dkong_audio_rptr, dkong_audio_wptr;
 
 // a total of 7 button is needed for
 // most games
-#define BUTTON_LEFT  0x01
-#define BUTTON_RIGHT 0x02
-#define BUTTON_UP    0x04
-#define BUTTON_DOWN  0x08
+#define BUTTON_LEFT  0x08
+#define BUTTON_RIGHT 0x04
+#define BUTTON_UP    0x01
+#define BUTTON_DOWN  0x02
 #define BUTTON_FIRE  0x10
 #define BUTTON_START 0x20
 #define BUTTON_COIN  0x40
-#define BUTTON_EXTRA 0x80
 
 #ifndef SINGLE_MACHINE
 enum {
